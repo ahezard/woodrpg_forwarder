ae4327368312f45bbd6ed5eadf2777e1b0fef2bb
ahezard <ahezard@gmail.com>
2016-07-17 19:04:18 +0200

    Header changed as for WoodR4_DSi.nds from apache

--- build_cia.sh
+++ build_cia.sh
@@ -1,9 +1,5 @@
 cp akmenu4/akmenu4_r4.nds akmenu4_r4_forwarder.nds
 
-#python patch_ndsheader_dsiware.py --read akmenu4_r4_forwarder.nds > akmenu4_r4_forwarder.nds_header.txt
-
-python patch_ndsheader_dsiware.py --title "AKMENU4" --maker 01 --code KAKE akmenu4_r4_forwarder.nds
-
-#python patch_ndsheader_dsiware.py --read akmenu4_r4_forwarder.nds > akmenu4_r4_forwarder.nds_header_patched.txt
+python patch_ndsheader_dsiware.py --title "AKMENU4" --maker01 --code AFFE akmenu4_r4_forwarder.nds
 
 ./make_cia --srl=akmenu4_r4_forwarder.nds
