ce14ed307fe76b9fa99fbb5130e518677d13e040
ahezard <ahezard@gmail.com>
2016-08-13 18:56:41 +0200

    Add forwarder building for acekard rpg

--- build_cia.sh
+++ build_cia.sh
@@ -9,3 +9,9 @@ cp akmenu4/_DSMENU.DAT akmenu4_r4idsn_forwarder.nds
 python patch_ndsheader_dsiware.py --title "WFWD R4IDSN" --maker 01 --code AFFE akmenu4_r4idsn_forwarder.nds
 
 ./make_cia --srl=akmenu4_r4idsn_forwarder.nds
+
+cp akmenu4/akmenu4.nds akmenu4_rpg_forwarder.nds
+
+python patch_ndsheader_dsiware.py --title "WFWD RPG" --maker 01 --code AFFE akmenu4_rpg_forwarder.nds
+
+./make_cia --srl=akmenu4_rpg_forwarder.nds
